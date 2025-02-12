320ec082408354de0e3c8e748397666314be1e0f
Sepehr <you@example.com>
2025-02-13 01:23:45 +0330

    added Request Logger

--- src/main/java/com/fanap/hotel/config/RequestLogger.java
+++ src/main/java/com/fanap/hotel/config/RequestLogger.java
@@ -18,5 +18,42 @@ import java.util.Enumeration;
 @Component
 public class RequestLogger {
 
+    private Logger logger = LoggerFactory.getLogger(RequestLogger.class);
 
+    @Pointcut("within(@org.springframework.web.bind.annotation.RestController *)")
+    public void restControllerPointCut(){
+
+    }
+
+    @Around("restControllerPointCut()")
+    public Object logRequestAndResponse(ProceedingJoinPoint joinPoint) throws Throwable{
+        HttpServletRequest httpServletRequest = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+
+        logger.info("------- Request Details --------");
+        logger.info("URL: {}", httpServletRequest.getRequestURL().toString());
+        logger.info("Http Method {}", httpServletRequest.getMethod());
+        logger.info("Headers: ");
+        Enumeration<String> headers = httpServletRequest.getHeaderNames();
+        while (headers.hasMoreElements()) {
+            String name = headers.nextElement();
+            logger.info("{}: {}", name, httpServletRequest.getHeader(name));
+        }
+
+        logger.info("Query Params = {}", httpServletRequest.getQueryString());
+
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        logger.info("Method Name, {}", signature.getMethod().getName());
+        logger.info("Arguments, {}", joinPoint.getArgs());
+
+        Object response;
+        try {
+            response = joinPoint.proceed();
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            throw e;
+        }
+
+        logger.info("Response, {}", response);
+        return response;
+    }
 }
